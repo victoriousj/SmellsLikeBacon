ad010cec80db7af380f192f2cc56ff62932f5683
VictoriousJ <victordjohnson@gmail.com>
2018-03-19 21:01:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/victor/smellslikebacon/ListFragment.java
@@ -0,0 +1,18 @@
+package com.example.victor.smellslikebacon;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment  extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container);
+        return view;
+    }
+}
