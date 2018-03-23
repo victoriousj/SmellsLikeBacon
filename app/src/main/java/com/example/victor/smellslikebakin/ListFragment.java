5c8a2a6bbf4074a1cd0ba9a79c0c1a987e9043f3
VictoriousJ <victordjohnson@gmail.com>
2018-03-22 21:59:06 -0500

    added the onClickListener for the list of recipes so that you can click on them.  Next is to bring users to an activity which shows the recipe, but for now we have a nice Toast as a proof of concept.

--- app/src/main/java/com/example/victor/smellslikebakin/ListFragment.java
+++ app/src/main/java/com/example/victor/smellslikebakin/ListFragment.java
@@ -11,13 +11,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class ListFragment  extends Fragment {
+
+    public interface OnRecipeSelectedInterface {
+        void onListRecipeSelected(int index);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        OnRecipeSelectedInterface listener = (OnRecipeSelectedInterface) getActivity();
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
         RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
-        ListAdapter listAdapter = new ListAdapter();
+        ListAdapter listAdapter = new ListAdapter(listener);
         recyclerView.setAdapter(listAdapter);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
