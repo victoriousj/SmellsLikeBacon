00279784a6b063de0ebb19586b54097cec554992
VictoriousJ <victordjohnson@gmail.com>
2018-03-22 22:00:34 -0500

    added a needed import that I forgot to include before the last push

--- app/src/main/java/com/example/victor/smellslikebakin/MainActivity.java
+++ app/src/main/java/com/example/victor/smellslikebakin/MainActivity.java
@@ -4,8 +4,9 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements ListFragment.OnRecipeSelectedInterface{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.placeHolder, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onListRecipeSelected(int index) {
+        Toast.makeText(this, Recipes.names[index], Toast.LENGTH_LONG).show();
     }
 }
