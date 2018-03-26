9fba64cc5b9e1ebe368185b3854823ed100c81fe
VictoriousJ <victordjohnson@gmail.com>
2018-03-25 19:59:39 -0500

    Added two new classes and layouts for the ViewPager that is going to get used for every ingredients so that we can view ingredients in one tab and the recipes directions in another

--- app/src/main/java/com/example/victor/smellslikebakin/ViewPagerFragment.java
+++ app/src/main/java/com/example/victor/smellslikebakin/ViewPagerFragment.java
@@ -4,6 +4,8 @@ package com.example.victor.smellslikebakin;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +19,25 @@ public class ViewPagerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         int index = getArguments().getInt(KEY_RECIPE_INDEX);
-//        Toast.makeText(getActivity(), Recipes.names[index], Toast.LENGTH_SHORT).show();
         getActivity().setTitle(Recipes.names[index]);
 
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        final IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        final DirectionsFragment directionsFragment = new DirectionsFragment();
+
+        ViewPager viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return position == 0 ? ingredientsFragment : directionsFragment;
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
         return view;
     }
 
